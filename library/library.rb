cbf84d330e0d49dabab3bdd049e96f511e0e134b
Graeme Cameron <gcameron4981@gmail.com>
2016-11-28 23:20:29 +0000

    Little bit more of Library done. not sure

--- library/library.rb
+++ library/library.rb
@@ -7,7 +7,18 @@ class Library
   end
 
   def details_on_book(book)
+    name = @book[book][:student_name]
+    date = @book[book][:date]
+
+    details = name + " " + date
+
+    return details
     
+  end
+
+  def add_book(new_book)
+    
+   Library.new({new_book.to_sym => {student_name:"",date:"" }})
   end
 
 end
--- library/specs/library_specs.rb
+++ library/specs/library_specs.rb
@@ -16,12 +16,15 @@ class LibrarySpec < MiniTest::Test
   def test_can_get_book_details()
     library = Library.new({LOTR:{student_name: "Jeff", date: "1/12/16" }})
 
-    details_on_book(LOTR:)
-
-    assert_equal("Jeff",)
-    assert_equal("1/12/16",)
+    assert_equal("Jeff 1/12/16",library.details_on_book(:LOTR))
 
   end
 
+  def test_add_book_to_library()
+    library = Library.new({LOTR:{student_name: "Jeff", date: "1/12/16" }})
+
+    library.add_book("BFG")
+    #assert_equal("   ",library.details_on_book(:BFG))
+  end
 
 end
